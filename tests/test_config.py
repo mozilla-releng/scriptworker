273706a0b1c6e46f37921f38b252bcd138342aad
Aki Sasaki <aki@escapewindow.com>
2016-04-25 13:52:16 -0700

    config 100% coverage

--- tests/test_config.py
+++ tests/test_config.py
@@ -3,6 +3,9 @@
 """Test scriptworker.config
 """
 from copy import deepcopy
+from frozendict import frozendict
+import json
+import os
 import pytest
 import scriptworker.config as config
 
@@ -18,19 +21,37 @@ class TestConfig(object):
         config.list_to_tuple(d)
         assert d == {'a': (1, 2, 3)}
 
-    def test_invalid_key(self, test_config):
+    def test_check_config_invalid_key(self, test_config):
         test_config['invalid_key_for_testing'] = 1
         messages = config.check_config(test_config, "test_path")
         assert "Unknown key" in "\n".join(messages)
 
-    def test_invalid_type(self, test_config):
+    def test_check_config_invalid_type(self, test_config):
         test_config['log_dir'] = tuple(test_config['log_dir'])
         messages = config.check_config(test_config, "test_path")
         assert "log_dir: type" in "\n".join(messages)
 
-    def test_valid_config(self, test_config):
+    def test_check_config_good(self, test_config):
         for key, value in test_config.items():
             if value == "...":
                 test_config[key] = "filled_in"
         messages = config.check_config(test_config, "test_path")
         assert messages == []
+
+    def test_create_config_missing_file(self):
+        with pytest.raises(SystemExit):
+            config.create_config("this_file_does_not_exist.json")
+
+    def test_create_config_bad_config(self):
+        path = os.path.join(os.path.dirname(__file__), "bad.json")
+        with pytest.raises(SystemExit):
+            config.create_config(path)
+
+    def test_create_config_good(self, test_config):
+        path = os.path.join(os.path.dirname(__file__), "good.json")
+        with open(path, "r") as fh:
+            contents = json.load(fh)
+        test_config.update(contents)
+        generated_config = config.create_config(path)
+        assert generated_config == frozendict(test_config)
+        assert isinstance(generated_config, frozendict)
