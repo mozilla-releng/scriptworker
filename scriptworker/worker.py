cd8b33138df419b82b120174eff6fb419b9a27a4
Aki Sasaki <aki@escapewindow.com>
2016-04-28 10:45:32 -0700

    got a 403 while calling azure poll urls

--- scriptworker/worker.py
+++ scriptworker/worker.py
@@ -9,6 +9,7 @@ from taskcluster.async import Queue
 from scriptworker.poll import find_task, get_azure_urls, update_poll_task_urls
 from scriptworker.config import create_config
 from scriptworker.context import Context
+from scriptworker.exceptions import ScriptWorkerException
 from scriptworker.log import update_logging_config
 from scriptworker.task import complete_task, run_task, schedule_reclaim_task, upload_artifacts
 from scriptworker.utils import cleanup, retry_request
@@ -27,8 +28,12 @@ async def async_main(context):
             args=(context.config['provisioner_id'], context.config['worker_type']),
         )
         for poll_url, delete_url in get_azure_urls(context):
-            task_defn = await find_task(context, poll_url, delete_url,
-                                        retry_request)
+            try:
+                task_defn = await find_task(context, poll_url, delete_url,
+                                            retry_request)
+            except ScriptWorkerException:
+                await asyncio.sleep(context.config['poll_interval'])
+                break
             if task_defn:
                 log.info("Going to run task!")
                 context.task = task_defn
@@ -41,8 +46,8 @@ async def async_main(context):
                 cleanup(context)
                 await asyncio.sleep(1)
                 break
-        else:
-            await asyncio.sleep(context.config['poll_interval'])
+            else:
+                await asyncio.sleep(context.config['poll_interval'])
 
 
 def main():
