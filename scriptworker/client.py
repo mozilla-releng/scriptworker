c5720d963017a67797ad16f2919a04e064fb2ced
Aki Sasaki <aki@escapewindow.com>
2016-05-04 22:54:59 -0700

    create task payload function

--- scriptworker/client.py
+++ scriptworker/client.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 """Jobs running in scriptworker will use functions in this file.
 """
+import arrow
 import json
 import jsonschema
 import os
@@ -28,3 +29,41 @@ def validate_task_schema(task, schema):
             "Can't validate task schema!",
             super_exc=exc
         )
+
+
+def integration_create_task_payload(config, task_group_id, scopes=None,
+                                    task_payload=None, task_extra=None):
+    """For various integration tests, we need to call createTask for test tasks.
+
+    This function creates a dummy payload for those createTask calls.
+    """
+    now = arrow.utcnow()
+    deadline = now.replace(hours=1)
+    expires = now.replace(days=3)
+    scopes = scopes or []
+    task_payload = task_payload or {}
+    task_extra = task_extra or {}
+    return {
+        'provisionerId': config['provisioner_id'],
+        'schedulerId': config['scheduler_id'],
+        'workerType': config['worker_type'],
+        'taskGroupId': task_group_id,
+        'dependencies': [],
+        'requires': 'all-completed',
+        'routes': [],
+        'priority': 'normal',
+        'retries': 5,
+        'created': now.isoformat(),
+        'deadline': deadline.isoformat(),
+        'expires': expires.isoformat(),
+        'scopes': scopes,
+        'payload': task_payload,
+        'metadata': {
+            'name': 'ScriptWorker Integration Test',
+            'description': 'ScriptWorker Integration Test',
+            'owner': 'release+python@mozilla.com',
+            'source': 'https://github.com/escapewindow/scriptworker/'
+        },
+        'tags': {},
+        'extra': task_extra,
+    }
