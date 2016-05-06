fbf8d8653f4749fe3848911a2d7c7a6375db5363
Aki Sasaki <aki@escapewindow.com>
2016-05-05 18:15:55 -0700

    most of client covered

--- tests/test_client.py
+++ tests/test_client.py
@@ -2,11 +2,18 @@
 # coding=utf-8
 """Test scriptworker.client
 """
-# import mock
+import arrow
+import mock
 import os
 import pytest
+from shutil import copyfile
 from scriptworker.exceptions import ScriptWorkerTaskException
 import scriptworker.client as client
+import scriptworker.utils as utils
+
+TEST_DATA_DIR = os.path.join(os.path.dirname(__file__), "data")
+PARTIAL_CREDS = os.path.join(TEST_DATA_DIR, "partial_credentials.json")
+CLIENT_CREDS = os.path.join(TEST_DATA_DIR, "client_credentials.json")
 
 
 @pytest.fixture(scope='function')
@@ -24,7 +31,35 @@ def config(tmpdir_factory):
     }
 
 
+def populate_credentials(config, sources, start=None):
+    start = start or arrow.utcnow().replace(minutes=-20)
+    for count, path in enumerate(sources):
+        new_time = start.replace(minutes=count)
+        copyfile(path, os.path.join(config['work_dir'], "credentials.{}.json".format(new_time.timestamp)))
+
+
+def no_sleep(*args, **kwargs):
+    return 0
+
+
 class TestClient(object):
     def test_get_missing_task(self, config):
         with pytest.raises(ScriptWorkerTaskException):
             client.get_task(config)
+
+    def test_get_task(self, config):
+        copyfile(
+            os.path.join(TEST_DATA_DIR, "basic_task.json"),
+            os.path.join(config['work_dir'], "task.json"),
+        )
+        assert client.get_task(config) == {"this_is_a_task": True}
+
+    def test_retry_fail_creds(self, config):
+        populate_credentials(config, [CLIENT_CREDS, PARTIAL_CREDS, PARTIAL_CREDS])
+        with mock.patch.object(utils, "calculateSleepTime", new=no_sleep):
+            with pytest.raises(ScriptWorkerTaskException):
+                client.get_temp_creds_from_file(config)
+
+    def test_get_missing_creds(self, config, event_loop):
+        with pytest.raises(ScriptWorkerTaskException):
+            event_loop.run_until_complete(client._get_temp_creds_from_file(config))
