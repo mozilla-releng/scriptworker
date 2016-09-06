1e5836643aa42beb284e5e73edd42a810d606669
Aki Sasaki <aki@escapewindow.com>
2016-09-05 20:16:05 -0700

    test importing multiple keys

--- scriptworker/test/test_gpg.py
+++ scriptworker/test/test_gpg.py
@@ -268,14 +268,31 @@ def test_generate_key(expires, expected):
 
 # import / export keys {{{1
 @pytest.mark.parametrize("suffix", (".pub", ".sec"))
-def test_import_keys(suffix):
+def test_import_single_key(suffix):
     with tempfile.TemporaryDirectory() as tmp:
         context = get_context(tmp)
         gpg = sgpg.GPG(context)
         with open("{}{}".format(KEYS_AND_FINGERPRINTS[0][2], suffix), "r") as fh:
             contents = fh.read()
         fingerprints = sgpg.import_keys(gpg, contents)
+        # the .sec fingerprints are doubled; use set() for unsorted & uniq
         assert set(fingerprints) == set([KEYS_AND_FINGERPRINTS[0][1]])
+
+
+@pytest.mark.parametrize("suffix", (".pub", ".sec"))
+def test_import_keys(suffix):
+    with tempfile.TemporaryDirectory() as tmp:
+        context = get_context(tmp)
+        gpg = sgpg.GPG(context)
+        contents = ""
+        expected = []
+        for kaf in KEYS_AND_FINGERPRINTS:
+            with open("{}{}".format(kaf[2], suffix), "r") as fh:
+                contents += fh.read()
+            expected.append(kaf[1])
+        fingerprints = sgpg.import_keys(gpg, contents)
+        # the .sec fingerprints are doubled; use set() for unsorted & uniq
+        assert set(fingerprints) == set(expected)
 
 
 @pytest.mark.parametrize("fingerprint,private,expected", EXPORT_KEY_PARAMS)
