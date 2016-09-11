69dc3ea6f3dd769dd3d8d4351370c4fb81b8403f
Aki Sasaki <aki@escapewindow.com>
2016-09-10 21:58:24 -0700

    add test files to package

--- setup.py
+++ setup.py
@@ -58,7 +58,7 @@ setup(
     author="Mozilla Release Engineering",
     author_email="release+python@mozilla.com",
     url="https://github.com/mozilla-releng/scriptworker",
-    packages=["scriptworker"],
+    packages=["scriptworker", "scriptworker.test"],
     package_data={"": ["version.json"]},
     include_package_data=True,
     entry_points={
