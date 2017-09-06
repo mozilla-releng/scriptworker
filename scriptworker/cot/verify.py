mozilla-releng/scriptworker

commit 0356bbb4b8e73c9af4d8a2ca7eec4e88cc0b97a6

Allow for `verify_cot` to verify any type of task

scriptworker/cot/verify.py
@@ -4,6 +4,7 @@
 Attributes:
     DECISION_MACH_COMMANDS (tuple): the allowlisted mach commands for a decision task,
         ignoring options.
+    DECISION_TASK_TYPES (tuple): the decision task types.
     log (logging.Logger): the log object for this module.
 
 """
@@ -42,6 +43,7 @@ DECISION_MACH_COMMANDS = ((
 ), (
     './mach', 'taskgraph', 'action-callback'
 ))
+DECISION_TASK_TYPES = ('decision', )
 
 
 # ChainOfTrust {{{1
@@ -118,6 +120,32 @@ class ChainOfTrust(object):
         if len(links) != 1:
             raise CoTError("No single Link matches task_id {}!\n{}".format(task_id, self.dependent_task_ids()))
         return links[0]
+
+    def is_decision(self):
+        """Determine if the chain is a decision task.
+
+        Returns:
+            bool: whether it is a decision task.
+
+        """
+        return self.task_type in DECISION_TASK_TYPES
+
+    def get_all_links_in_chain(self):
+        """Return all links in the chain of trust, including the target task.
+
+        By default, we're checking a task and all its dependencies back to the
+        tree, so the full chain is ``self.links`` + ``self``. However, we also
+        support checking the decision task itself. In that case, we populate
+        the decision task as a link in ``self.links``, and we don't need to add
+        another check for ``self``.
+
+        Returns:
+            list: of all ``LinkOfTrust``s to verify.
+
+        """
+        if self.is_decision() and self.get_link(self.task_id):
+            return self.links
+        return [self] + self.links
 
 
 # LinkOfTrust {{{1
@@ -509,11 +537,10 @@ def find_sorted_task_dependencies(task, task_name, task_id):
     dependencies = _sort_dependencies_by_name_then_task_id(dependencies)
 
     decision_task_id = get_decision_task_id(task)
-    task_type = guess_task_type(task_name)
-    if decision_task_id != task_id and task_type != 'decision':
-        # make sure we deal with the decision task first, or we may populate
-        # signing:build0:decision before signing:decision
-        dependencies.insert(0, _craft_dependency_tuple(task_name, 'decision', decision_task_id))
+    # make sure we deal with the decision task first, or we may populate
+    # signing:build0:decision before signing:decision
+    decision_tuple = _craft_dependency_tuple(task_name, 'decision', decision_task_id)
+    dependencies.insert(0, decision_tuple)
 
     log.info('found dependencies: {}'.format(dependencies))
     return dependencies
@@ -950,7 +977,7 @@ async def verify_decision_task(chain, link):
     link.task_graph = load_json(
         path, is_path=True, exception=CoTError, message="Can't load {}! %(exc)s".format(path)
     )
-    for target_link in [chain] + chain.links:
+    for target_link in chain.get_all_links_in_chain():
         # Verify the target's task is in the decision task's task graph, unless
         # it's this task or another decision task.
         # https://github.com/mozilla-releng/scriptworker/issues/77
@@ -1123,8 +1150,7 @@ async def verify_task_types(chain):
     """
     valid_task_types = get_valid_task_types()
     task_count = {}
-    # check the chain object (current task) as well
-    for obj in [chain] + chain.links:
+    for obj in chain.get_all_links_in_chain():
         task_type = obj.task_type
         log.info("Verifying {} {} as a {} task...".format(obj.name, obj.task_id, task_type))
         task_count.setdefault(task_type, 0)
@@ -1195,14 +1221,12 @@ async def verify_worker_impls(chain):
 
     """
     valid_worker_impls = get_valid_worker_impls()
-    for obj in [chain] + chain.links:
+    for obj in chain.get_all_links_in_chain():
         worker_impl = obj.worker_impl
         log.info("Verifying {} {} as a {} task...".format(obj.name, obj.task_id, worker_impl))
         # Run tests synchronously for now.  We can parallelize if efficiency
         # is more important than a single simple logfile.
         await valid_worker_impls[worker_impl](chain, obj)
-        if isinstance(obj, ChainOfTrust) and obj.worker_impl != "scriptworker":
-            raise CoTError("ChainOfTrust object is not a scriptworker impl!")
 
 
 # get_firefox_source_url {{{1
@@ -1381,7 +1405,7 @@ To use, first either set your taskcluster creds in your env http://bit.ly/2eDMa6
 or in the CREDS_FILES http://bit.ly/2fVMu0A""")
     parser.add_argument('task_id', help='the task id to test')
     parser.add_argument('--task-type', help='the task type to test',
-                        choices=['signing', 'balrog', 'beetmover', 'pushapk'], required=True)
+                        choices=sorted(get_valid_task_types().keys()), required=True)
     parser.add_argument('--cleanup', help='clean up the temp dir afterwards',
                         dest='cleanup', action='store_true', default=False)
     opts = parser.parse_args(args)

scriptworker/test/test_cot_verify.py
@@ -198,6 +198,20 @@ def test_dependent_task_ids(chain):
     assert sorted(chain.dependent_task_ids()) == sorted(ids)
 
 
+# get_all_links_in_chain {{{1
+@pytest.mark.asyncio
+async def test_get_all_links_in_chain(chain, decision_link, build_link):
+    # standard test
+    chain.links = [build_link, decision_link]
+    assert set(chain.get_all_links_in_chain()) == set([chain, decision_link, build_link])
+
+    # decision task test
+    chain.task_type = 'decision'
+    chain.task_id = decision_link.task_id
+    chain.links = [build_link, decision_link]
+    assert set(chain.get_all_links_in_chain()) == set([decision_link, build_link])
+
+
 # is_try {{{1
 @pytest.mark.parametrize("bools,expected", (([False, False], False), ([False, True], True)))
 def test_chain_is_try(chain, bools, expected):
@@ -379,12 +393,14 @@ def test_verify_docker_image_sha_bad_allowlist(chain, build_link, decision_link,
         cotverify.verify_docker_image_sha(chain, decision_link)
 
 
-@pytest.mark.parametrize("task,expected", ((
+@pytest.mark.parametrize("task,expected,task_type", ((
     {'taskGroupId': 'task_id', 'extra': {}, 'payload': {}},
-    []
+    [('decision:decision', 'task_id')],
+    'decision'
 ), (
     {'taskGroupId': 'decision_task_id', 'extra': {}, 'payload': {}},
-    [('build:decision', 'decision_task_id')]
+    [('build:decision', 'decision_task_id')],
+    'build'
 ), (
     {
         'taskGroupId': 'decision_task_id',
@@ -395,7 +411,8 @@ def test_verify_docker_image_sha_bad_allowlist(chain, build_link, decision_link,
     }, [
         ('build:decision', 'decision_task_id'),
         ('build:docker-image', 'docker_image_task_id'),
-    ]
+    ],
+    'build'
 ), (
     {
         'taskGroupId': 'decision_task_id',
@@ -416,7 +433,8 @@ def test_verify_docker_image_sha_bad_allowlist(chain, build_link, decision_link,
         ('build:blah', 'blah_task_id'),
         ('build:blah', 'blah_task_id'),     # Duplicates aren't deleted
         ('build:docker-image', 'docker_image_task_id'),
-    ]
+    ],
+    'build'
 ), (
     # PushAPK-like definitions
     {
@@ -435,14 +453,15 @@ def test_verify_docker_image_sha_bad_allowlist(chain, build_link, decision_link,
             }],
         },
     }, [
-        ('build:decision', 'decision_task_id'),
-        ('build:signing', 'platform_0_signing_task_id'),
-        ('build:signing', 'platform_1_signing_task_id'),
-        ('build:signing', 'platform_2_signing_task_id'),
-    ]
+        ('pushapk:decision', 'decision_task_id'),
+        ('pushapk:signing', 'platform_0_signing_task_id'),
+        ('pushapk:signing', 'platform_1_signing_task_id'),
+        ('pushapk:signing', 'platform_2_signing_task_id'),
+    ],
+    'pushapk'
 )))
-def test_find_sorted_task_dependencies(task, expected):
-    assert expected == cotverify.find_sorted_task_dependencies(task, 'build', 'task_id')
+def test_find_sorted_task_dependencies(task, expected, task_type):
+    assert expected == cotverify.find_sorted_task_dependencies(task, task_type, 'task_id')
 
 
 # build_task_dependencies {{{1
@@ -979,19 +998,13 @@ async def test_verify_generic_worker_task(mocker):
 
 
 # verify_worker_impls {{{1
-@pytest.mark.parametrize("raises", (True, False))
 @pytest.mark.asyncio
 async def test_verify_worker_impls(chain, decision_link, build_link,
-                                   docker_image_link, raises, mocker):
+                                   docker_image_link, mocker):
     chain.links = [decision_link, build_link, docker_image_link]
     for func in cotverify.get_valid_worker_impls().values():
         mocker.patch.object(cotverify, func.__name__, new=noop_async)
-    if raises:
-        chain.worker_impl = 'docker-worker'
-        with pytest.raises(CoTError):
-            await cotverify.verify_worker_impls(chain)
-    else:
-        await cotverify.verify_worker_impls(chain)
+    await cotverify.verify_worker_impls(chain)
 
 
 # get_firefox_source_url {{{1

commit 74b365a26447efad299cd61f8a09021782c7f1e4

bug 1396517 - don't add a decision task's decision task to cot
The current assumption is decision tasks should be standalone and
verifiable as-is. This is fallout from
https://github.com/mozilla-releng/scriptworker/commit/0356bbb4b8e73c9af4d8a2ca7eec4e88cc0b97a6#diff-340a1f42e56ecb68f523d2badfa58686L513 .

I was able to run `verify_cot` against the failing fennec signing task
and its decision task; they now pass.

scriptworker/cot/verify.py
@@ -509,6 +509,7 @@ def verify_docker_image_sha(chain, link):
     raise_on_errors(errors)
 
 
+# find_sorted_task_dependencies {{{1
 def find_sorted_task_dependencies(task, task_name, task_id):
     """Find the taskIds of the chain of trust dependencies of a given task.
 
@@ -536,7 +537,13 @@ def find_sorted_task_dependencies(task, task_name, task_id):
     dependencies = [*cot_input_dependencies, *upstream_artifacts_dependencies]
     dependencies = _sort_dependencies_by_name_then_task_id(dependencies)
 
-    decision_task_id = get_decision_task_id(task)
+    # bug 1396517 - Decision tasks are standalone; don't point at another task
+    # even if the taskGroupId doesn't match.
+    task_type = guess_task_type(task_name)
+    if task_type in DECISION_TASK_TYPES:
+        decision_task_id = task_id
+    else:
+        decision_task_id = get_decision_task_id(task)
     # make sure we deal with the decision task first, or we may populate
     # signing:build0:decision before signing:decision
     decision_tuple = _craft_dependency_tuple(task_name, 'decision', decision_task_id)

scriptworker/test/test_cot_verify.py
@@ -393,8 +393,10 @@ def test_verify_docker_image_sha_bad_allowlist(chain, build_link, decision_link,
         cotverify.verify_docker_image_sha(chain, decision_link)
 
 
+# find_sorted_task_dependencies{{{1
 @pytest.mark.parametrize("task,expected,task_type", ((
-    {'taskGroupId': 'task_id', 'extra': {}, 'payload': {}},
+    # Make sure we don't follow other_task_id on a decision task
+    {'taskGroupId': 'other_task_id', 'extra': {}, 'payload': {}},
     [('decision:decision', 'task_id')],
     'decision'
 ), (