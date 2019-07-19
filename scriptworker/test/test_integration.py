58ce964c3e9f8d5829689e823152d3639a6487e7
Tom Prince <mozilla@hocat.ca>
2019-07-18 18:24:43 -0600

    Split production CoT tests into separate module.
    This also changes them to be able to run without credentials.

--- scriptworker/test/test_integration.py
+++ scriptworker/test/test_integration.py
@@ -10,7 +10,6 @@ import json
 import logging
 import os
 import pytest
-import re
 
 from scriptworker.exceptions import Download404
 import slugid
@@ -23,12 +22,10 @@ from scriptworker.config import (
 )
 from scriptworker.constants import DEFAULT_CONFIG
 from scriptworker.context import Context
-from scriptworker.cot.verify import ChainOfTrust, verify_chain_of_trust
 import scriptworker.log as swlog
 import scriptworker.artifacts as artifacts
 import scriptworker.worker as worker
 import scriptworker.utils as utils
-from taskcluster.aio import Index, Queue
 from . import integration_create_task_payload
 
 log = logging.getLogger(__name__)
@@ -330,91 +327,6 @@ async def test_temp_creds(context_function):
             )
             result = await context.temp_queue.ping()
             assert result['alive']
-
-
-# verify_cot {{{1
-@pytest.mark.skipif(os.environ.get("NO_TESTS_OVER_WIRE"), reason=SKIP_REASON)
-@pytest.mark.parametrize("branch_context", ({
-    "name": "mozilla-central nightly desktop",
-    "index": "gecko.v2.mozilla-central.latest.firefox.decision-nightly-desktop",
-    "task_label_to_task_type": {
-        "balrog-.*-nightly/opt": "balrog",
-        "beetmover-repackage-.*-nightly/opt": "beetmover",
-    },
-}, {
-    "name": "mozilla-central nightly android",
-    "index": "gecko.v2.mozilla-central.latest.firefox.decision-nightly-android",
-    "task_label_to_task_type": {
-        "push-apk/opt": "pushapk",
-    },
-}, {
-    "name": "mozilla-central win64 en-US repackage signing",
-    "index": "gecko.v2.mozilla-central.nightly.latest.firefox.win64-nightly-repackage-signing",
-    "task_type": "signing",
-}, {
-    "name": "mozilla-beta win64 en-US repackage signing",
-    "index": "gecko.v2.mozilla-beta.nightly.latest.firefox.win64-nightly-repackage-signing",
-    "task_type": "signing",
-}, {
-    "name": "mozilla-release win64 en-US repackage signing",
-    "index": "gecko.v2.mozilla-release.nightly.latest.firefox.win64-nightly-repackage-signing",
-    "task_type": "signing",
-}, {
-    "name": "mozilla-esr60 win64 en-US repackage signing",
-    "index": "gecko.v2.mozilla-esr60.nightly.latest.firefox.win64-nightly-repackage-signing",
-    "task_type": "signing",
-}))
-@pytest.mark.asyncio
-async def test_verify_production_cot(branch_context):
-    index = Index(options={'rootUrl': DEFAULT_CONFIG['taskcluster_root_url']})
-    queue = Queue(options={'rootUrl': DEFAULT_CONFIG['taskcluster_root_url']})
-
-    async def get_task_id_from_index(index_path):
-        res = await index.findTask(index_path)
-        return res['taskId']
-
-    async def get_completed_task_info_from_labels(decision_task_id, label_to_task_type):
-        label_to_taskid = await queue.getLatestArtifact(
-            decision_task_id, "public/label-to-taskid.json"
-        )
-        task_info = {}
-        for re_label, task_type in label_to_task_type.items():
-            r = re.compile(re_label)
-            for label, task_id in label_to_taskid.items():
-                if r.match(label):
-                    status = await queue.status(task_id)
-                    # only run verify_cot against tasks with completed deps.
-                    if status['status']['state'] in ('completed', 'running', 'pending', 'failed'):
-                        task_info[task_id] = task_type
-                        break
-            else:
-                log.warning(
-                    "Not running verify_cot against {} {} because there are no elegible completed tasks".format(
-                        decision_task_id, task_type
-                    )
-                )
-        return task_info
-
-    async def verify_cot(name, task_id, task_type):
-        log.info("Verifying {} {} {}...".format(name, task_id, task_type))
-        async with get_context() as context:
-            context.task = await queue.task(task_id)
-            cot = ChainOfTrust(context, task_type, task_id=task_id)
-            await verify_chain_of_trust(cot)
-
-    task_id = await get_task_id_from_index(branch_context['index'])
-    assert task_id, "{}: Can't get task_id from index {}!".format(
-        branch_context['name'], branch_context['index']
-    )
-    if branch_context.get('task_label_to_task_type'):
-        task_info = await get_completed_task_info_from_labels(
-            task_id, branch_context['task_label_to_task_type']
-        )
-        for task_id, task_type in task_info.items():
-            name = "{} {}".format(branch_context['name'], task_type)
-            await verify_cot(name, task_id, task_type)
-    else:
-        await verify_cot(branch_context['name'], task_id, branch_context['task_type'])
 
 
 # private artifacts {{{1
