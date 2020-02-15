aa5debca395bb2712ed672b134de1041faec6805
Aki Sasaki <aki@escapewindow.com>
2020-02-14 17:43:31 -0800

    add py38-cot; add py38 support in package desc

--- setup.py
+++ setup.py
@@ -111,5 +111,6 @@ setup(
         "Natural Language :: English",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
     ),
 )
