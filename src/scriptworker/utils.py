ae47f4dece608ddddc57723799a4f6bfd7d940cd
Justin Wood <Callek@gmail.com>
2021-03-22 15:09:26 -0400

    Add typing to context.py

--- src/scriptworker/utils.py
+++ src/scriptworker/utils.py
@@ -25,6 +25,7 @@ import async_timeout
 import yaml
 from taskcluster.client import createTemporaryCredentials
 
+from scriptworker.context import Context
 from scriptworker.exceptions import Download404, DownloadError, ScriptWorkerException, ScriptWorkerRetryException, ScriptWorkerTaskException
 
 log = logging.getLogger(__name__)
@@ -710,7 +711,13 @@ def get_parts_of_url_path(url):
 
 
 # load_json_or_yaml_from_url {{{1
-async def load_json_or_yaml_from_url(context, url, path, overwrite=True, auth=None):
+async def load_json_or_yaml_from_url(
+    context: Context,
+    url: str,
+    path: str,
+    overwrite: bool=True,
+    auth: Optional[str]=None
+) -> Dict[str, Any]:
     """Retry a json/yaml file download, load it, then return its data.
 
     Args:
