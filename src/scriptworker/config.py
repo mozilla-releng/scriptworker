a5f11287255e2d11e2a95fae29f76f65a5635227
Aki Sasaki <aki@mozilla.com>
2021-08-05 16:05:35 -0700

    address collections deprecation warning

--- src/scriptworker/config.py
+++ src/scriptworker/config.py
@@ -12,7 +12,7 @@ import logging
 import os
 import re
 import sys
-from collections import Mapping
+from collections.abc import Mapping
 from copy import deepcopy
 
 from immutabledict import immutabledict
