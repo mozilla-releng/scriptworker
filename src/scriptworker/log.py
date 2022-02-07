14bb48fe89d2c57040b3a8558a9435dcb3dfa870
Heitor Neiva <hneiva@mozilla.com>
2022-02-07 15:47:43 -0800

    Add log rotation handler

--- src/scriptworker/log.py
+++ src/scriptworker/log.py
@@ -57,6 +57,12 @@ def update_logging_config(context: Any, log_name: Optional[str] = None, file_nam
         # If we rotate the log file via logrotate.d, let's watch the file
         # so we can automatically close/reopen on move.
         handler = logging.handlers.WatchedFileHandler(path)
+    elif "log_max_bytes" in context.config or "log_max_backups" in context.config:
+        handler = logging.handlers.RotatingFileHandler(
+            path=path,
+            maxBytes=context.config.get('log_max_bytes', 0),
+            backupCount=context.config.get('log_max_backups', 0),
+        )
     else:
         # Avoid using WatchedFileHandler during scriptworker unittests
         handler = logging.FileHandler(path)
