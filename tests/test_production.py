c8a4e83a88b7631f0494f0abe63102f42d2c05a6
Aki Sasaki <aki@mozilla.com>
2022-07-27 11:02:24 -0700

    fix cot test

--- tests/test_production.py
+++ tests/test_production.py
@@ -166,9 +166,9 @@ VERIFY_COT_BRANCH_CONTEXTS = (
         "cot_product": "adhoc",
     },
     {
-        "name": "xpi-manifest reset-search-default",
+        "name": "xpi-manifest firefox-translations",
         "taskcluster_root_url": "https://firefox-ci-tc.services.mozilla.com/",
-        "index": "xpi.v2.xpi-manifest.reset-search-default-extension.release-signing.latest",
+        "index": "xpi.v2.xpi-manifest.firefox-translations.release-signing.latest",
         "task_type": "signing",
         "cot_product": "xpi",
     },
