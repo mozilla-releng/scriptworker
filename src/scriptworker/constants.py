c7b79d9a7f302ab357a841920bf31a406bc0c8fc
Andrew Halberstadt <ahal@mozilla.com>
2022-07-27 14:33:02 -0400

    Add docker-worker CoT key and release scriptworker 43.3.0 (#549)
    * RELENG-830 - Add 2022 docker-worker trusted CoT public key
    
    * scriptworker 43.3.0

--- src/scriptworker/constants.py
+++ src/scriptworker/constants.py
@@ -83,7 +83,14 @@ DEFAULT_CONFIG: immutabledict[str, Any] = immutabledict(
         "ed25519_private_key_path": "...",
         "ed25519_public_keys": immutabledict(
             {
-                "docker-worker": tuple(["1cnK7Qc2wjL9Dl7XTBgNE9Ns+NWHraCE5qfxblEKg8A="]),
+                "docker-worker": tuple(
+                    [
+                        # 2021 key; remove when we've finished migrating to the new key
+                        "1cnK7Qc2wjL9Dl7XTBgNE9Ns+NWHraCE5qfxblEKg8A=",
+                        # 2022 key, RELENG-830
+                        "YM4whpr6pGNnF+LHUGWZPgDXiWOTdEQwctTr4LyeO/Q=",
+                    ]
+                ),
                 "generic-worker": tuple(
                     [
                         # 2021 key; remove when we've finished migrating to the new key
