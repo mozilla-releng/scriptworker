a0957db19f2d38f3ed58655dd1f2992a44422dbb
Julien Cristau <jcristau@mozilla.com>
2023-10-03 16:52:20 +0200

    Remove {mozilla,comm}-esr102 support (bug 1854084) (#622)

--- tests/test_task.py
+++ tests/test_task.py
@@ -197,7 +197,7 @@ def test_get_worker_type(task, result):
     (
         ("https://hg.mozilla.org/mozilla-central", "mozilla-central", False, "firefox"),
         ("https://hg.mozilla.org/projects/foo", "foo", True, "firefox"),
-        ("https://hg.mozilla.org/releases/mozilla-esr102", "mozilla-esr102", False, "firefox"),
+        ("https://hg.mozilla.org/releases/mozilla-esr102", "mozilla-esr102", True, "firefox"),
         ("https://hg.mozilla.org/releases/mozilla-esr115", "mozilla-esr115", False, "firefox"),
         ("https://hg.mozilla.org/try", "try", False, "firefox"),
         ("https://hg.mozilla.org/releases/unknown", "unknown", True, "firefox"),
