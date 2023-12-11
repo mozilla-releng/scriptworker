mozilla-releng/scriptworker

commit 9c778ef514f118a7bc3fc5a1e6da2b716830e88d

Bug 1860092 - Add comm-release support to scriptworker. (#624)
* Bug 1860092 - Add comm-release support to scriptworker.

* Bug 1860092 - Reformat with black.

src/scriptworker/constants.py
@@ -220,7 +220,7 @@ DEFAULT_CONFIG: immutabledict[str, Any] = immutabledict(
                             {
                                 "schemes": ("https", "ssh"),
                                 "netlocs": ("hg.mozilla.org",),
-                                "path_regexes": (r"^(?P<path>/comm-central)(/|$)", r"^(?P<path>/releases/comm-(beta|esr\d+))(/|$)"),
+                                "path_regexes": (r"^(?P<path>/comm-central)(/|$)", r"^(?P<path>/releases/comm-(beta|release|esr\d+))(/|$)"),
                             }
                         ),
                     ),
@@ -365,6 +365,7 @@ DEFAULT_CONFIG: immutabledict[str, Any] = immutabledict(
                         {
                             "project:comm:thunderbird:releng:balrog:server:nightly": "all-nightly-branches",
                             "project:comm:thunderbird:releng:balrog:server:beta": "beta",
+                            "project:comm:thunderbird:releng:balrog:server:release": "release",
                             "project:comm:thunderbird:releng:balrog:server:esr": "esr",
                             "project:comm:thunderbird:releng:beetmover:bucket:nightly": "all-nightly-branches",
                             "project:comm:thunderbird:releng:beetmover:bucket:release": "all-release-branches",
@@ -477,13 +478,16 @@ DEFAULT_CONFIG: immutabledict[str, Any] = immutabledict(
                         {
                             "all-release-branches": (
                                 "/releases/comm-beta",
+                                "/releases/comm-release",
                                 "/releases/comm-esr115",
                             ),
                             "beta": ("/releases/comm-beta",),
+                            "release": ("/releases/comm-release",),
                             "esr": ("/releases/comm-esr115",),
                             "all-nightly-branches": (
                                 "/comm-central",
                                 "/releases/comm-beta",
+                                "/releases/comm-release",
                                 "/releases/comm-esr115",
                             ),
                             "nightly": ("/comm-central",),

commit fe5de415edb3b10cd7050f1092d5aaea157bcb71

Bug 1869123 - Allow comm-esr115 to use the "balrog:server:release" restricted scope.
Fixes a regression from PR #624.

This is a temporary situation while Thunderbird monthly releases are in development.
Until comm-esr115 is migrated to use a (new) "esr" update channel, it needs to
use the "release" channel.

Details why comm-esr115 was able to use "release" prior to now are in the bug.

src/scriptworker/constants.py
@@ -371,7 +371,7 @@ DEFAULT_CONFIG: immutabledict[str, Any] = immutabledict(
                         {
                             "project:comm:thunderbird:releng:balrog:server:nightly": "all-nightly-branches",
                             "project:comm:thunderbird:releng:balrog:server:beta": "beta",
-                            "project:comm:thunderbird:releng:balrog:server:release": "release",
+                            "project:comm:thunderbird:releng:balrog:server:release": "release-or-esr",
                             "project:comm:thunderbird:releng:balrog:server:esr": "esr",
                             "project:comm:thunderbird:releng:beetmover:bucket:nightly": "all-nightly-branches",
                             "project:comm:thunderbird:releng:beetmover:bucket:release": "all-release-branches",
@@ -489,6 +489,7 @@ DEFAULT_CONFIG: immutabledict[str, Any] = immutabledict(
                             ),
                             "beta": ("/releases/comm-beta",),
                             "release": ("/releases/comm-release",),
+                            "release-or-esr": ("/releases/comm-release", "/releases/comm-esr115"),
                             "esr": ("/releases/comm-esr115",),
                             "all-nightly-branches": (
                                 "/comm-central",