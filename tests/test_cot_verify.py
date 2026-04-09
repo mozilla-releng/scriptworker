mozilla-releng/scriptworker

commit 8b6ab79d2fa40a61148b50da28c3b907ce4a776c

When checking chain-of-trust after the fact, check the task being verified as well.
When scriptworker is running, the task whose chain is being verified isn't
complete, so not all the checks can be applied to it. When the production CoT
verification tests, or the CLI is check a task, the checks can be applied to
the task itself as well.

src/scriptworker/cot/verify.py
@@ -554,6 +554,33 @@ def _sort_dependencies_by_name_then_task_id(dependencies):
 
 
 # build_task_dependencies {{{1
+async def build_link(chain, task_name, task_id):
+    """Build a LinkOfTrust and add it to the chain.
+
+    Args:
+        chain (ChainOfTrust): the chain of trust to add to.
+        task_name (str): the name of the task to operate on.
+        task_id (str): the taskId of the task to operate on.
+
+    Raises:
+        CoTError: on failure.
+
+    """
+    link = LinkOfTrust(chain.context, task_name, task_id)
+    json_path = link.get_artifact_full_path("task.json")
+    try:
+        task_defn = await chain.context.queue.task(task_id)
+        link.task = task_defn
+        chain.links.append(link)
+        # write task json to disk
+        makedirs(os.path.dirname(json_path))
+        with open(json_path, "w") as fh:
+            fh.write(format_json(task_defn))
+        await build_task_dependencies(chain, task_defn, task_name, task_id)
+    except TaskclusterFailure as exc:
+        raise CoTError(str(exc))
+
+
 async def build_task_dependencies(chain, task, name, my_task_id):
     """Recursively build the task dependencies of a task.
 
@@ -574,19 +601,7 @@ async def build_task_dependencies(chain, task, name, my_task_id):
 
     for task_name, task_id in sorted_dependencies:
         if task_id not in chain.dependent_task_ids():
-            link = LinkOfTrust(chain.context, task_name, task_id)
-            json_path = link.get_artifact_full_path("task.json")
-            try:
-                task_defn = await chain.context.queue.task(task_id)
-                link.task = task_defn
-                chain.links.append(link)
-                # write task json to disk
-                makedirs(os.path.dirname(json_path))
-                with open(json_path, "w") as fh:
-                    fh.write(format_json(task_defn))
-                await build_task_dependencies(chain, task_defn, task_name, task_id)
-            except TaskclusterFailure as exc:
-                raise CoTError(str(exc))
+            await build_link(chain, task_name, task_id)
 
 
 # download_cot {{{1
@@ -1854,11 +1869,13 @@ class AuditLogFormatter(logging.Formatter):
 
 
 # verify_chain_of_trust {{{1
-async def verify_chain_of_trust(chain):
+async def verify_chain_of_trust(chain, *, check_task=False):
     """Build and verify the chain of trust.
 
     Args:
         chain (ChainOfTrust): the chain we're operating on
+        check_task (bool): Whether to download and verify the task itself. This
+            is useful for verifying a task after it has run.
 
     Raises:
         CoTError: on failure
@@ -1875,7 +1892,10 @@ async def verify_chain_of_trust(chain):
         log.info("Running scriptworker version {}".format(__version_string__))
         try:
             # build LinkOfTrust objects
-            await build_task_dependencies(chain, chain.task, chain.name, chain.task_id)
+            if check_task:
+                await build_link(chain, chain.name, chain.task_id)
+            else:
+                await build_task_dependencies(chain, chain.task, chain.name, chain.task_id)
             # download the signed chain of trust artifacts
             await download_cot(chain)
             # verify the signatures and populate the ``link.cot``s
@@ -1918,7 +1938,7 @@ async def _async_verify_cot_cmdln(opts, tmp):
         if os.environ.get("SCRIPTWORKER_GITHUB_OAUTH_TOKEN"):
             context.config["github_oauth_token"] = os.environ.get("SCRIPTWORKER_GITHUB_OAUTH_TOKEN")
         cot = ChainOfTrust(context, opts.task_type, task_id=opts.task_id)
-        await verify_chain_of_trust(cot)
+        await verify_chain_of_trust(cot, check_task=True)
         log.info(format_json(cot.dependent_task_ids()))
         log.info("{} : {}".format(cot.name, cot.task_id))
         for link in cot.links:

tests/test_cot_verify.py
@@ -2036,21 +2036,22 @@ def test_audit_log_formatter(level, expected):
 
 # verify_chain_of_trust {{{1
 @pytest.mark.parametrize("exc", (None, KeyError, TypeError, CoTError))
+@pytest.mark.parametrize("check_task", (True, False))
 @pytest.mark.asyncio
-async def test_verify_chain_of_trust(chain, exc, mocker):
+async def test_verify_chain_of_trust(chain, exc, check_task, mocker):
     async def maybe_die(*args):
         if exc is not None:
             raise exc("blah")
 
-    for func in ("build_task_dependencies", "download_cot", "download_cot_artifacts", "verify_task_types", "verify_worker_impls"):
+    for func in ("build_task_dependencies", "build_link", "download_cot", "download_cot_artifacts", "verify_task_types", "verify_worker_impls"):
         mocker.patch.object(cotverify, func, new=noop_async)
     mocker.patch.object(cotverify, "verify_cot_signatures", new=noop_sync)
     mocker.patch.object(cotverify, "trace_back_to_tree", new=maybe_die)
     if exc:
         with pytest.raises(CoTError):
-            await cotverify.verify_chain_of_trust(chain)
+            await cotverify.verify_chain_of_trust(chain, check_task=check_task)
     else:
-        await cotverify.verify_chain_of_trust(chain)
+        await cotverify.verify_chain_of_trust(chain, check_task=check_task)
 
 
 # verify_cot_cmdln {{{1

tests/test_production.py
@@ -160,7 +160,7 @@ async def test_verify_production_cot(branch_context):
         log.info("Verifying {} {} {}...".format(name, task_id, task_type))
         context.task = await queue.task(task_id)
         cot = ChainOfTrust(context, task_type, task_id=task_id)
-        await verify_chain_of_trust(cot)
+        await verify_chain_of_trust(cot, check_task=True)
 
     async with get_context({"cot_product": branch_context["cot_product"]}) as context:
         context.queue = queue

commit f3c0f29ba56c30063011db2e2f7e31e22dd2e0be

Don't return the root of a CoT twice when it's also a link
Creating the CoT here, with `no_check_task` being default (False)
https://github.com/mozilla-releng/scriptworker/blob/6b0b66b03e9ec07b462a550b19b742a863d6b17a/src/scriptworker/cot/verify.py#L2089-L2091

Then because `check_task` is true, it calls `build_link` with itself,
adding a `LinkOfTrust` with the same task ID.
https://github.com/mozilla-releng/scriptworker/blob/6b0b66b03e9ec07b462a550b19b742a863d6b17a/src/scriptworker/cot/verify.py#L2044-L2045

Then we call `verify_task_types` on that chain, which calls
`get_all_links_in_chain` and actually verifies links. That's where the
log is coming from.
https://github.com/mozilla-releng/scriptworker/blob/6b0b66b03e9ec07b462a550b19b742a863d6b17a/src/scriptworker/cot/verify.py#L1806-L1813

The problem is that `get_all_links_in_chain` returns `[self] +
self.links` if the `CoT` object isn't a decision task
https://github.com/mozilla-releng/scriptworker/blob/6b0b66b03e9ec07b462a550b19b742a863d6b17a/src/scriptworker/cot/verify.py#L212

So it ends up returning the root task twice, one because of `[self]` and
once because `check_task` added the root as a `LoT` to the `CoT`.

---

`get_all_links_in_chain` was added in
0356bbb4b8e73c9af4d8a2ca7eec4e88cc0b97a6 but at the time, `check_task`
didn't exist so it was correct. The bug was introduced by
8b6ab79d2fa40a61148b50da28c3b907ce4a776c which started adding the root
of a `CoT` as a link, making `get_all_links_in_chain` return "dupes" for
it.

This most likely never got caught because it only happens when
check_task is True which isn't the case for scriptworkers, only
`verify_cot` and only when checking a non decision task.

This fixes the issue by not relying on the task type to know whether or
not it should return itself in the chain but by looking down the graph
to see if it's already included or not.

src/scriptworker/cot/verify.py
@@ -222,7 +222,7 @@ class ChainOfTrust(object):
             list: of all ``LinkOfTrust``s to verify.
 
         """
-        if self.is_decision() and self.get_link(self.task_id):
+        if self.task_id in self.dependent_task_ids():
             return self.links
         return [self] + self.links
 

tests/test_cot_verify.py
@@ -362,6 +362,12 @@ async def test_get_all_links_in_chain(chain, decision_link, build_link):
     chain.links = [build_link, decision_link]
     assert set(chain.get_all_links_in_chain()) == set([decision_link, build_link])
 
+    # Ensure we don't return the root twice when `check_task` is True for non decision tasks
+    chain.task_type = "signing"
+    self_link = cotverify.LinkOfTrust(chain.context, chain.name, chain.task_id)
+    chain.links = [build_link, decision_link, self_link]
+    assert set(chain.get_all_links_in_chain()) == set([build_link, decision_link, self_link])
+
 
 # is_try {{{1
 @pytest.mark.asyncio